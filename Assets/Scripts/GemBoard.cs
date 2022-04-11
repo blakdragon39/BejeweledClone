8bc4cf5334096c4fba676dab9413d10f6c6d576f
Blakdragon39 <jenna.hartley@live.ca>
2022-04-11 13:03:19 -0600

    Started dropping gems once a match is made, but it's a mess

--- Assets/Scripts/GemBoard.cs
+++ Assets/Scripts/GemBoard.cs
@@ -1,6 +1,10 @@
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GemBoard : MonoBehaviour {
 
@@ -15,6 +19,7 @@ public class GemBoard : MonoBehaviour {
 
     [SerializeField] private int boardSize;
     [SerializeField] private int gemSize;
+    [SerializeField] private float swapSpeed;
 
     private List<GameObject> gemOptions;
     private Gem[][] board;
@@ -50,21 +55,32 @@ public class GemBoard : MonoBehaviour {
                        (y >= 2 && board[x][y - 1].GemType == type && board[x][y-2].GemType == type)) {
                     type = Random.Range(0, gemOptions.Count);
                 }
-                
-                var gemPrefab = gemOptions[type];
-                var prefab = Instantiate(gemPrefab);
-                prefab.SetActive(true);
-                prefab.transform.SetParent(transform, false);
-                prefab.transform.localPosition = new Vector3(x * gemSize, -(y * gemSize), -5);
-                
-                var gem = prefab.GetComponent<Gem>();
-                gem.GemType = type;
-                gem.SetBoardPosition(x, y);
-                gem.OnGemClick += OnGemClick;
-                
-                board[x][y] = gem;
+
+                var newGem = GenerateNewGem(type, x, y);
+                newGem.gameObject.transform.localPosition = new Vector3(x * gemSize, -(y * gemSize), -5);
             }
         }
+
+        GetComponent<RectTransform>().sizeDelta = new Vector2(boardSize * gemSize, boardSize * gemSize);
+    }
+    
+    private Gem GenerateNewGem(int type, int x, int y) {
+        var prefab = Instantiate(gemOptions[type]);
+        var gem = prefab.GetComponent<Gem>();
+        
+        gem.gameObject.SetActive(true);
+        gem.gameObject.transform.SetParent(transform, false);
+        gem.GemType = type;
+        gem.OnGemClick += OnGemClick;
+        
+        SetGemBoardPosition(gem, x, y);
+
+        return gem;
+    }
+
+    private void SetGemBoardPosition(Gem gem, int x, int y) {
+        gem.SetBoardPosition(x, y);
+        board[x][y] = gem;
     }
 
     private void OnGemClick(int x, int y) {
@@ -79,7 +95,7 @@ public class GemBoard : MonoBehaviour {
             selectedGem = null;
         } else if (selectedGem.X == x + 1 || selectedGem.X == x - 1 || 
                    selectedGem.Y == y + 1 || selectedGem.Y == y - 1) {
-            SwapGems(nextSelectedGem);
+            StartCoroutine(SwapGems(nextSelectedGem));
         } else {
             selectedGem.SetSpinning(false);
             nextSelectedGem.SetSpinning(true);
@@ -87,38 +103,63 @@ public class GemBoard : MonoBehaviour {
         }
     }
 
-    private void SwapGems(Gem swapGem) {
-        var tempPos = selectedGem.transform.position;
+    private IEnumerator SwapGems(Gem swapGem) {
+        var tempPos = selectedGem.transform.localPosition;
         var tempX = selectedGem.X;
         var tempY = selectedGem.Y;
 
-        //todo animate swapping
-        selectedGem.transform.position = swapGem.transform.position;
+        var tween = selectedGem.transform.DOLocalMove(swapGem.transform.localPosition, swapSpeed);
         selectedGem.SetBoardPosition(swapGem.X, swapGem.Y);
         board[swapGem.X][swapGem.Y] = selectedGem;
 
-        swapGem.transform.position = tempPos;
+        swapGem.transform.DOLocalMove(tempPos, swapSpeed);
         swapGem.SetBoardPosition(tempX, tempY);
         board[tempX][tempY] = swapGem;
         
         selectedGem.SetSpinning(false);
         selectedGem = null;
 
-        CheckForMatches();
+        yield return tween.WaitForCompletion();
+        yield return CheckForMatches();
     }
 
-    private void CheckForMatches() {
+    private IEnumerator CheckForMatches() {
         var matches = FindMatches();
-
         foreach (var match in matches) {
             board[match.X][match.Y] = null;
             match.gameObject.SetActive(false);
             Destroy(match.gameObject);
         }
-        //destroy each game object
-        //move all higher gems downwards
-        //create new gems in empty spots
-        //check for more cascading matches
+
+        yield return MoveGemsDown(matches.ToList());
+        //todo check for more cascading matches
+    }
+
+    private IEnumerator MoveGemsDown(List<Gem> missingGems) {
+        TweenerCore<Vector3, Vector3, VectorOptions> tween = null;
+        missingGems.Sort((gem1, gem2) => gem2.Y - gem1.Y);
+
+        foreach (var gem in missingGems) {
+            Gem newGem = null;
+            for (int y = gem.Y - 1; y >= 0; y--) {
+                newGem = board[gem.X][y];
+
+                if (newGem != null) break;
+            }
+
+            if (newGem == null) {
+                var type = Random.Range(0, gemOptions.Count);
+                newGem = GenerateNewGem(type, gem.X, gem.Y);
+                //todo set better position above top of board:
+                newGem.gameObject.transform.localPosition = new Vector3(gem.X * gemSize, gemSize, -5);
+            } else {
+                SetGemBoardPosition(newGem, gem.X, gem.Y);
+            }
+
+            tween = newGem.transform.DOLocalMoveY(-(gem.Y * gemSize), swapSpeed);
+        }
+
+        yield return tween.WaitForCompletion();
     }
 
     private HashSet<Gem> FindMatches() {
